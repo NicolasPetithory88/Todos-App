b288a388406b6fad380fefa5a3cfc85baeccfb9a
Nicolas PETITHORY <nicolas4.petithory@gmail.com>
2022-12-31 01:56:34 +0100

    add+delete todo / style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,28 @@
 import React from "react";
+import styles from "./TodoItem.module.css"
 
 //! Class based component
 class TodoItem extends React.Component {
     render() {
-        return <li>{this.props.todo.title}</li>
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+        }
+        
+        const { completed, id, title } = this.props.todo
+
+        return <li className={styles.item}>
+            <input 
+                type="checkbox" 
+                className={styles.checkbox}
+                checked={completed} 
+                onChange={() => this.props.handleChangeProps(id)}
+            />
+            <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+            <span style={completed ? completedStyle : null}>{title}</span>
+        </li>
       }
 }
 
