74195916eaa1ea29fcaf1197866d7ff26a1897f2
Nicolas PETITHORY <nicolas4.petithory@gmail.com>
2022-12-31 06:59:40 +0100

    update todo / fetch

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,28 @@
 import React from "react";
 import styles from "./TodoItem.module.css"
 
-//! Class based component
+//! Class based component, we use it cause we hold data in the state, otherwise we could use a function based component
 class TodoItem extends React.Component {
+    state = {
+        editing: false,
+      }
+
+    handleEditing = () => {
+        this.setState({
+        editing: true,
+        })
+    }
+
+    handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+            this.setState({ editing: false })
+          }
+      }
+
+    componentWillUnmount() {
+        console.log("Cleaning up...")
+    }
+
     render() {
         const completedStyle = {
             fontStyle: "italic",
@@ -11,24 +31,40 @@ class TodoItem extends React.Component {
             textDecoration: "line-through",
         }
         
-        const { completed, id, title } = this.props.todo
+        const { completed, id, title } = this.props.todo;
+
+        let viewMode = {}
+        let editMode = {}
+
+        if (this.state.editing) {
+        viewMode.display = "none"
+        } else {
+        editMode.display = "none"
+        }
 
         return <li className={styles.item}>
+            <div onDoubleClick={this.handleEditing} style={viewMode}>
+                <input 
+                    type="checkbox" 
+                    className={styles.checkbox}
+                    checked={completed} 
+                    onChange={() => this.props.handleChangeProps(id)}
+                />
+                <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+                <span style={completed ? completedStyle : null}>{title}</span>
+            </div>
             <input 
-                type="checkbox" 
-                className={styles.checkbox}
-                checked={completed} 
-                onChange={() => this.props.handleChangeProps(id)}
+                type="text" 
+                style={editMode} 
+                className={styles.textInput} 
+                value={title}
+                onChange={e => {
+                    this.props.setUpdate(e.target.value, id)
+                  }}
+                onKeyDown={this.handleUpdatedDone}    
             />
-            <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-            <span style={completed ? completedStyle : null}>{title}</span>
         </li>
       }
 }
-
-//! Function based component (possible cause this component doesnt hold any data, data is held by TodoContainerComponent)
-// function TodoItem(props) {
-//     return <li>{props.todo.title}</li>
-//   }
   
 export default TodoItem
